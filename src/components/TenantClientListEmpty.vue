f35d3b1161b71e6d9b948487dd346dc1a0c0f148
spark <parkyngj@gmail.com>
2020-05-23 17:28:52 -0700

    feat: add component for empty state of tenant's client list

---
+++ src/components/TenantClientListEmpty.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="text-black">
+    <div class="tg-h2-mobile mb-4">Manage Your Clients</div>
+    <div class="tg-body-mobile mb-4">
+      Access your client’s information, photos, and agreements here.
+    </div>
+    <div>
+      <BaseButton class="bg-secondary text-on-primary">
+        <IconAdd slot="icon" class="fill-current" />
+        <span slot="cta-text">Add New Client</span>
+      </BaseButton>
+    </div>
+  </div>
+</template>
+
+<script>
+import BaseButton from '@/components/BaseButton.vue';
+import IconAdd from '@/assets/icons/add.svg';
+
+export default {
+  name: 'TenantClientListEmpty',
+  components: {
+    BaseButton,
+    IconAdd
+  }
+};
+</script>
