afdb85a0bce490594980a84155efaa0e0e84c499
giteshk11 <writemail2gitesh@gmail.com>
2020-10-09 22:47:57 +0530

    added splash screen. #415

---
+++ src/components/Splash.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="h-full flex flex-col justify-center items-center bg-primary">
+    <div class="max-w-xl h-full">
+      <img
+        src="https://res.cloudinary.com/whynotearth/image/upload/v1598336510/BrowTricks/static_v2/brandcover_lo3i0l.jpg"
+        class="h-screen md:max-h-screen object-cover"
+      />
+    </div>
+    <div class="absolute w-screen h-full flex justify-center items-center">
+      <BaseSpinner borderColor="border-brand2" classNames="spinner" />
+    </div>
+  </div>
+</template>
+
+<script>
+import BaseSpinner from '@/components/BaseSpinner.vue';
+
+export default {
+  name: 'Splash',
+  components: {
+    BaseSpinner
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,46 +1,64 @@
 <template>
-  <SplashLayout>
-    <template #image>
-      <img
-        class="logo mb-8"
-        src="https://res.cloudinary.com/whynotearth/image/upload/v1600846670/BrowTricks/static_v2/logo-round-wbg_oo659o.png"
-        alt=""
-      />
-    </template>
-    <template #pretitle>Welcome to</template>
-    <template #title>{{ SHORTNAME }}</template>
-    <template #primaryCTA>
-      <Button
-        background="bg-primary"
-        textColor="text-on-primary"
-        title="Create Account"
-        class="normal-case mb-2"
-        :to="{ name: 'AuthSignup' }"
-      />
-    </template>
-    <template #tertiaryCTA>
-      <p class="text-on-background">
-        Already have an account?
-        <router-link class="" :to="{ name: 'AuthLogin' }">Log In</router-link>
-      </p>
-    </template>
-  </SplashLayout>
+  <div class="w-full h-screen">
+    <transition mode="out-in" v-if="isSplash">
+      <Splash />
+    </transition>
+    <SplashLayout v-else>
+      <template #image>
+        <img
+          class="logo mb-8"
+          src="https://res.cloudinary.com/whynotearth/image/upload/v1600846670/BrowTricks/static_v2/logo-round-wbg_oo659o.png"
+          alt=""
+        />
+      </template>
+      <template #pretitle>Welcome to</template>
+      <template #title>{{ SHORTNAME }}</template>
+      <template #primaryCTA>
+        <Button
+          background="bg-primary"
+          textColor="text-on-primary"
+          title="Create Account"
+          class="normal-case mb-2"
+          :to="{ name: 'AuthSignup' }"
+        />
+      </template>
+      <template #tertiaryCTA>
+        <p class="text-on-background">
+          Already have an account?
+          <router-link class="" :to="{ name: 'AuthLogin' }">Log In</router-link>
+        </p>
+      </template>
+    </SplashLayout>
+  </div>
 </template>
 
 <script>
 import SplashLayout from '@/layouts/SplashLayout.vue';
+import Splash from '@/components/Splash.vue';
 import { mapGetters, mapActions } from 'vuex';
+const SPLASH_TIMEOUT = 1500;
 
 export default {
   name: 'Home',
   components: {
-    SplashLayout
+    SplashLayout,
+    Splash
+  },
+  data() {
+    return {
+      isSplash: true
+    };
   },
   computed: {
     ...mapGetters('auth', ['isAuthenticated']),
     SHORTNAME() {
       return process.env.VUE_APP_SHORTNAME;
     }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.isSplash = false;
+    }, SPLASH_TIMEOUT);
   },
   methods: {
     ...mapActions('global', ['isDrawerOpenUpdate', 'isDrawerOpenAuthUpdate'])
