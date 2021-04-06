73bc13295895d4c4efa6ddb92ca7714c83f2dd44
Atharva Sharma <mailatharvasharma@gmail.com>
2021-04-06 09:59:23 +0530

    feat: article changes

--- src/components/ArticleMini.vue
+++ src/components/ArticleMini.vue
@@ -1,10 +1,10 @@
 <template functional>
-  <article class="mx-auto max-w-xl px-2 md:px-16 text-center lg:text-left">
+  <article class="mx-auto max-w-xl px-8 text-center md:text-left">
     <h2
       class="mb-6 md:mb-8 tg-home-h2-mobile md:tg-home-h2-desktop"
       v-text="props.article.title"
     />
-    <p class="tg-home-body" v-text="props.article.content" />
+    <p class="tg-body-mobile" v-text="props.article.content" />
   </article>
 </template>
 
