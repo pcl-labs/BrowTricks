70c692eed185d0a0a8be47f4eadd328284bb8ce6
Atharva Sharma <mailatharvasharma@gmail.com>
2021-04-19 18:12:55 +0530

    feat: phone image

--- tailwind.config.js
+++ tailwind.config.js
@@ -57,10 +57,7 @@ module.exports = {
       },
       backgroundImage: {
         'phones-image':
-          'url(https://res.cloudinary.com/whynotearth/image/upload/v1618759425/BrowTricks/Phones_Full_Width_qedj14.png)',
-        // Need path to be relative to the app.css where tailwind is located
-        spots: 'url(../patterns/spots.png)',
-        patch: 'url(../patterns/patch.png)'
+          'url(https://res.cloudinary.com/whynotearth/image/upload/v1618759425/BrowTricks/Phones_Full_Width_qedj14.png)'
       }
     }
   },
