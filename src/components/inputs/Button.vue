aa98b3d11853d1d91ae2d8882e3491113f528ac9
Atharva Sharma <mailatharvasharma@gmail.com>
2021-04-25 03:22:37 +0530

    feat: improved loading and added to login

--- src/components/inputs/Button.vue
+++ src/components/inputs/Button.vue
@@ -135,9 +135,11 @@ export default {
   /* color is brand1 */
   box-shadow: 0 0 0 2px #b5bbc9;
 }
-.button:disabled {
+.button:disabled,
+.button:disabled:hover {
   @apply bg-opacity-disabled;
   @apply cursor-not-allowed;
+  @apply shadow-none;
 }
 .min-w-200 {
   min-width: 200px;
