f2b95d16235ef492fc22de1cbe90b22b796b94b2
Atharva Sharma <mailatharvasharma@gmail.com>
2021-04-25 00:55:29 +0530

    feat: super admin page

--- src/views/SuperAdmin.vue
+++ src/views/SuperAdmin.vue
@@ -3,25 +3,19 @@
     <div class="pb-20">
       <HeaderHeroSection>
         <!-- header -->
-        <div>
-          <h1
-            class="tg-h2-mobile text-opacity-high text-on-background uppercase flex mb-2 items-center"
-          >
-            <img class="mb-4" :src="logo" alt="user-logo" />
+        <div class="flex flex-col mb-8 items-center">
+          <img class="h-20 mx-auto" :src="logo" alt="logo" />
+          <h1 class="tg-h2-mobile text-opacity-high text-on-background">
+            Welcome Back Boss!
           </h1>
         </div>
 
         <!-- content -->
-        <router-link
-          :to="{ name: 'SuperAdminTenantsList' }"
-          class="max-w-md mx-auto w-full h-full"
-        >
-          <ExpansionPanel title="Tenants">
-            <template #preIcon>
-              <IconUser class="h-4 w-4 fill-current" />
-            </template>
-          </ExpansionPanel>
-        </router-link>
+        <div class="mb-8 max-w-md mx-auto w-full h-full">
+          <router-link :to="{ name: 'SuperAdminTenantsList' }">
+            <ExpansionPanel title="Tenants" />
+          </router-link>
+        </div>
       </HeaderHeroSection>
     </div>
   </PageContentBoard>
@@ -31,19 +25,18 @@
 import HeaderHeroSection from '@/components/HeaderHeroSection.vue';
 import PageContentBoard from '@/components/PageContentBoard.vue';
 import ExpansionPanel from '@/components/ExpansionPanel.vue';
-import IconUser from '@/assets/icons/user-nocircle.svg';
 
 export default {
   name: 'SuperAdmin',
-  data: () => ({
-    isTutorialsModalOpen: false,
-    logo: process.env.VUE_APP_LOGO3_URL
-  }),
+  data() {
+    return {
+      logo: process.env.VUE_APP_LOGO2_URL
+    };
+  },
   components: {
     PageContentBoard,
     HeaderHeroSection,
-    ExpansionPanel,
-    IconUser
+    ExpansionPanel
   }
 };
 </script>
