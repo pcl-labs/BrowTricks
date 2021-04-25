1973862b5b4571faf9f28ef1eac8d4cbe07ac2ef
Atharva Sharma <mailatharvasharma@gmail.com>
2021-04-26 02:36:23 +0530

    feat: infinite loading and search client list and overlay

--- src/views/ClientList.vue
+++ src/views/ClientList.vue
@@ -1,10 +1,18 @@
 <template>
   <PageContentBoard>
-    <div class="my-4 p-4" v-if="clients.length > 0">
+    <div class="my-4 px-4">
+      <div class="search-box">
+        <MaterialInput
+          v-model="query"
+          :immediate-input="true"
+          label="Search"
+          placeholder="Search by name, number or email"
+        />
+      </div>
       <div v-for="(client, key) in clients" :key="key">
         <h6
           v-if="showLetter(clients[key - 1], client)"
-          class="p-3 tg-caption-mobile"
+          class="tg-caption-mobile"
         >
           {{ client.firstName && client.firstName[0].toUpperCase() }}
         </h6>
@@ -18,45 +26,34 @@
           <ClientListItem :client="client" />
         </router-link>
       </div>
-    </div>
-
-    <div v-else class="p-6 text-center max-w-sm mx-auto">
-      <p class="tg-body-mobile text-on-background text-opacity-high mb-6">
-        Add your first clients.
-      </p>
-
-      <Button
-        class="mb-4"
-        background="bg-brand2"
-        title="Get Started"
-        :to="{ name: 'ClientItemAdd' }"
-      />
+      <infinite-loading :identifier="infiniteId" @infinite="fetchClients" />
     </div>
 
     <portal to="HeaderAction">
       <router-link
-        class="tg-color-label-mobile block py-1 px-2"
+        class="tg-color-label-mobile flex flex-row items-center py-1 px-2"
         :to="{
           name: 'ClientItemAdd'
         }"
-        title="Add Client"
       >
-        <IconEdit class="text-background text-opacity-high fill-current" />
+        <IconAdd class="mr-2 text-background text-opacity-high fill-current" />
+        Add client
       </router-link>
     </portal>
   </PageContentBoard>
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex';
 import ClientListItem from '@/components/client/ClientListItem';
 import PageContentBoard from '@/components/PageContentBoard';
-import IconEdit from '@/assets/icons/edit.svg';
+import IconAdd from '@/assets/icons/add.svg';
+import { ClientService } from '@whynotearth/meredith-axios';
+import { debounce } from 'lodash-es';
 
 export default {
   name: 'ClientList',
   components: {
-    IconEdit,
+    IconAdd,
     PageContentBoard,
     ClientListItem
   },
@@ -68,19 +65,20 @@ export default {
   },
   data() {
     return {
-      client: false,
       logoUrl:
-        'https://res.cloudinary.com/whynotearth/image/upload/v1585738963/BrowTricks/_0003_MANAGE-CLIENTS_hl5cux.png'
+        'https://res.cloudinary.com/whynotearth/image/upload/v1585738963/BrowTricks/_0003_MANAGE-CLIENTS_hl5cux.png',
+      clients: [],
+      page: 0,
+      query: '',
+      infiniteId: +new Date()
     };
   },
-  async created() {
-    this.loadingUpdate(true);
-    await this.fetchClients(this.tenantSlug);
-    this.loadingUpdate(false);
+  watch: {
+    query() {
+      this.reset();
+    }
   },
   methods: {
-    ...mapActions('loading', ['loadingUpdate']),
-    ...mapActions('client', ['fetchClients']),
     showLetter(prev, current) {
       if (!prev) return true;
 
@@ -90,10 +88,38 @@ export default {
         current && current.firstName && current.firstName[0].toUpperCase();
 
       return getPrevFirstCharacter !== getCurrentFirstCharacter;
-    }
-  },
-  computed: {
-    ...mapGetters('client', ['clients'])
+    },
+    fetchClients($state) {
+      this.loading = true;
+      return ClientService.clients1({
+        companySlug: process.env.VUE_APP_COMPANY_SLUG,
+        tenantSlug: this.tenantSlug,
+        page: this.page,
+        query: this.query
+      })
+        .then(response => {
+          const clients = response.records;
+          if (clients.length) {
+            this.page += 1;
+            this.clients.push(...clients);
+            $state.loaded();
+          } else {
+            $state.complete();
+          }
+        })
+        .catch(() => {
+          $state.error();
+        });
+    },
+    reset: debounce(
+      function() {
+        this.page = 0;
+        this.clients = [];
+        this.infiniteId += 1;
+      },
+      300,
+      { maxWait: 3000 }
+    )
   }
 };
 </script>
