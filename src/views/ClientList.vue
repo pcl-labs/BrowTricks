53e3fe5a930822eb4eb3bef1f6a6c4865c4029b5
Atharva Sharma <mailatharvasharma@gmail.com>
2021-04-26 02:36:23 +0530

    feat: infinite loading and search client list and overlay

--- src/views/ClientList.vue
+++ src/views/ClientList.vue
@@ -1,10 +1,18 @@
 <template>
   <PageContentBoard>
-    <div class="my-4 p-4" v-if="clients.length > 0">
+    <div class="my-4 px-4">
+      <div class="search-box">
+        <MaterialInput
+          v-model="query"
+          :immediate-input="true"
+          label="Search"
+          placeholder="Search by name, number or email"
+        />
+      </div>
       <div v-for="(client, key) in clients" :key="key">
         <h6
           v-if="showLetter(clients[key - 1], client)"
-          class="p-3 tg-caption-mobile"
+          class="tg-caption-mobile"
         >
           {{ client.firstName && client.firstName[0].toUpperCase() }}
         </h6>
