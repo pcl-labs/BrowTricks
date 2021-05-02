8d760fde899aaa097022df2cb02cd08c167bc328
Gitesh <writemail2gitesh@gmail.com>
2021-05-02 22:17:33 +0530

    feat: created Add Payment Method with add card info & billing info forms

---
+++ src/components/paymentMethods/AddCardInfoForm.vue
@@ -0,0 +1,53 @@
+<template>
+  <BaseCard className="flex-col gap-2">
+    <MaterialInput
+      placeholder="First Name"
+      label="First Name"
+      labelColor="text-on-background text-opacity-50"
+    >
+    </MaterialInput>
+    <MaterialInput
+      placeholder="Last Name"
+      label="last Name"
+      labelColor="text-on-background text-opacity-50"
+    >
+    </MaterialInput>
+    <MaterialInput
+      placeholder="Card Number"
+      label="Card Number"
+      labelColor="text-on-background text-opacity-50"
+    >
+    </MaterialInput>
+    <MaterialInput
+      placeholder="Expiration Date (MM/YY)"
+      label="Expiration Date (MM/YY)"
+      labelColor="text-on-background text-opacity-50"
+    >
+    </MaterialInput>
+    <MaterialInput
+      placeholder="Security Code (CCV)"
+      label="Security Code (CCV)"
+      labelColor="text-on-background text-opacity-50"
+    >
+    </MaterialInput>
+    <MaterialInput
+      placeholder="Zip Code"
+      label="Zip Code"
+      labelColor="text-on-background text-opacity-50"
+    >
+    </MaterialInput>
+  </BaseCard>
+</template>
+
+<script>
+import BaseCard from '@/components/BaseCard.vue';
+import MaterialInput from '@/components/inputs/MaterialInput.vue';
+
+export default {
+  name: 'AddCardInfoForm',
+  components: {
+    BaseCard,
+    MaterialInput
+  }
+};
+</script>
---
+++ src/views/PaymentMethods.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="p-4 space-y-4">
+    <PaymentMethodCard> </PaymentMethodCard>
+    <Button
+      title="+ Add Payment Methods"
+      border="border-dashed border-2 border-brand6 border-opacity-50"
+      width="w-full"
+      textColor="text-brand6 text-opacity-50"
+      @clicked="showAddPaymentMethodForms"
+    >
+    </Button>
+    <div class="pb-16 space-y-4" v-show="isFormVisible">
+      <AddCardInfoForm> </AddCardInfoForm>
+      <AddBillingInfoForm></AddBillingInfoForm>
+      <Button title="Save" background="bg-white"> </Button>
+    </div>
+  </div>
+</template>
+
+<script>
+import PaymentMethodCard from '@/components/paymentMethods/PaymentMethodCard.vue';
+import AddCardInfoForm from '@/components/paymentMethods/AddCardInfoForm.vue';
+import AddBillingInfoForm from '@/components/paymentMethods/AddBillingInfoForm.vue';
+
+export default {
+  name: 'PaymentMethods',
+  components: {
+    PaymentMethodCard,
+    AddCardInfoForm,
+    AddBillingInfoForm
+  },
+  data() {
+    return {
+      isFormVisible: false
+    };
+  },
+  methods: {
+    showAddPaymentMethodForms() {
+      this.isFormVisible = true;
+    }
+  }
+};
+</script>
