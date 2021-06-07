5fdbc0465122d58f8aebe0ddbd772594765d7015
Atharva Sharma <mailatharvasharma@gmail.com>
2021-06-08 03:18:44 +0530

    feat: improvements to ActivityDetailsRow

--- src/components/activity/ActivityDetailsRow.vue
+++ src/components/activity/ActivityDetailsRow.vue
@@ -3,35 +3,36 @@
     :href="details.statementLink"
     target="_blank"
     title="View transaction invoice"
-    class="w-full flex justify-around py-2 gap-2 text cursor-pointer"
+    class="w-full flex justify-around py-2 gap-2 hover:bg-brand8 hover:bg-opacity-50 cursor-pointer"
   >
-    <IconRectangle />
+    <AttachMoney class="fill-current flex-shrink-0 text-brand3" />
     <div class="flex-1 flex flex-col items-start">
       <div class="flex w-full justify-between">
-        <p :class="[nameStyles]">
+        <span :class="[nameStyles]">
           {{ formatDate(details.paymentDate) }}
-        </p>
-        <div :class="[totalFontStyles, 'self-end']">$ {{ details.total }}</div>
+        </span>
+        <span :class="[totalFontStyles, 'mx-2 self-end']">
+          ${{ details.total }}
+        </span>
       </div>
-      <div
+      <p
         class="w-full flex gap-1 items-center flex-no-wrap tg-caption-mobile text-brand3 truncate"
       >
-        <div>Subscription</div>
-        <div :class="[dotBg, 'rounded-full w-2 h-2 mx-1']" />
-        <div>{{ details.transactionId }}</div>
-      </div>
+        <span :class="[dotBg, 'rounded-full w-2 h-2 mx-0.5']" />
+        Subscription
+      </p>
     </div>
   </a>
 </template>
 
 <script>
-import IconRectangle from '@/assets/icons/Rectangle.svg';
+import AttachMoney from '@/assets/icons/attach_money.svg';
 import { format } from 'date-fns';
 
 export default {
   name: 'ActivityDetailsRow',
   components: {
-    IconRectangle
+    AttachMoney
   },
   props: {
     details: {
